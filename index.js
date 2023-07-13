bcac64484405e47ab0a55fad57fffa1836609273
camilleramos1 <129894673+camilleramos1@users.noreply.github.com>
2023-07-13 17:11:49 -0600

    started functions for user selection

--- index.js
+++ index.js
@@ -13,14 +13,15 @@ const db = mysql.createConnection(
       password: 'Atlas123!',
       database: 'employeeTracker_db'
     },
-    db.connect((err) => {
-        if (err) {
-            throw err;
-        }
-        console.log(`Connected to the employeeTracker_db database.`);
-        questions();
-    })
 );
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Connected to the employeeTracker_db database.`);
+    questions();
+});
 // prompts for user to select from
 function questions() {
     inquirer.prompt([
@@ -65,12 +66,48 @@ function questions() {
     });
 };
 
-// function to view all departments
-const viewDepartments = () => {
-    const action = `SELECT department_name FROM department`
+// function to view all departments name and id
+function viewDepartments() {
+    let action = `SELECT department.id AS Id, department_name AS Department FROM department`;
     db.query(action, (err, res) => {
         if (err) throw err;
         console.table(res)
+        questions();
+    })
+};
+// function to view all roles including role id, salary, title, and dept name
+function viewRoles() {
+    let action = `SELECT R.id AS Id, R.title AS Title, R.salary AS Salary, department.department_name AS Department 
+    FROM role R JOIN department ON role.department_id = department.id order by role.id ASC;`
+    db.query(action, (err,res) => {
+        if (err) throw err;
+        console.table(res);
+        questions();
     })
 };
 
+function viewEmployees() {
+    let action = `SELECT 
+    E.id AS Id, 
+    E.first_name AS "First Name", 
+    E.last_name AS "Last Name", 
+    role.title AS Title, 
+    role.salary AS Salary, 
+    department.department_name AS Department, 
+    CONCAT(m.first_name,' ',m.last_name) AS Manager
+    FROM employee E 
+    INNER JOIN role ON role_id = role.id 
+    INNER JOIN department ON department.id = role.department_id
+    LEFT JOIN employee m on e.manager_id = m.id
+    order by E.id;`
+    db.query(action, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        questions();
+    })
+};
+
+// function updateEmployeeRole() {
+//     db.query
+// }
+// db.query
